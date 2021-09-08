853385aa88dc0a360b86278df057cd968f309be1
alde zaw <htunthant.zaw.54@gmail.com>
2021-09-08 10:30:49 +0700

    completed

---
+++ Assingment2.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setINV <- function(solve) inv <<- solve
+    getINV <- function() inv
+    list(set = set, get = get,
+         setINV = setINV,
+         getINV = getINV)
+  }
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getINV()
+  if(!is.null(inv)) {
+    message("getting cached matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setINV(inv)
+  inv
+}
